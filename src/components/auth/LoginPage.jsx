9804677faa0d19244fe73e0d2d709a0387244d3b
acehidan <aungaungoogame2@gmail.com>
2025-12-17 21:50:24 +0630

    update login page header text from "Sign in to UEDC" to "Sign in to Dashboard" for clarity

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -48,7 +48,7 @@ const LoginPage = () => {
     <div className="flex w-full justify-center items-center h-screen">
       <div className="w-full md:w-[450px] bg-white rounded-lg p-20 md:p-6">
         <h2 className="header font-bold mb-10 border-b pb-5">
-          Sign in to UEDC
+          Sign in to Dashboard
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
